6d6e7cadaee6bf725de4ff7e16a5af8d585ca7a1
kerryblue14 <kerryblue14@gmail.com>
2021-02-23 21:10:38 +0000

    change weather icon
    changed weather icon to change with location.

--- src/weather-script.js
+++ src/weather-script.js
@@ -44,19 +44,25 @@ let currentDate = document.querySelector("#date");
 currentDate.innerHTML = formatDate(now);
 
 function currentTemperature(response) {
+	console.log(response.data);
 	let temperatureElement = document.querySelector("#temperature");
 	let cityElement = document.querySelector("#submitCity");
 	let descriptionElement = document.querySelector("#description");
 	let humidityElement = document.querySelector("#humidity");
 	let windspeedElement = document.querySelector("#speed");
+	let iconElement = document.querySelector("#icon");
 	temperatureElement.innerHTML = Math.round(response.data.main.temp);
 	cityElement.innerHTML = response.data.name;
 	descriptionElement.innerHTML = response.data.weather[0].description;
 	humidityElement.innerHTML = response.data.main.humidity;
 	windspeedElement.innerHTML = Math.round(response.data.wind.speed);
+	iconElement.setAttribute(
+		"src",
+		`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+	);
 }
 
-let city = "Galway";
+let city = "London";
 let units = "metric";
 
 let apiKey = "350541b21f9e750e54359106bf7f6f0d";
